54c5121428303098257d0a7ec6e4eea8231b6d63
JunMac <junnehoomaki@yahoo.co.jp>
2023-03-23 17:10:39 +0900

    cross origin 変更

--- backend/main.py
+++ backend/main.py
@@ -26,11 +26,12 @@ origins = [
     "http://127.0.0.1:8080",
     "http://localhost:8080",
     "http://192.168.0.10:8080",
+    "http://192.168."
 
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
