46e2245b458970d1abfe2fbf4e358ef3d809ff15
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2023-03-27 13:01:07 +0900

    v-card調整

--- frontend/src/TopMenus.vue
+++ frontend/src/TopMenus.vue
@@ -1,42 +1,56 @@
+<script setup>
+const toCodeMenu = () => {
+  window.location.href = "/code/";
+};
+
+const toTreeMenu = () => {
+  window.location.href = "/tree/";
+};
+</script>
 <template>
   <div>
     <h1>メニュー</h1>
-    <v-row justify="start">
-      <v-col>
-        <v-card class="mx-auto" max-width="344" variant="outlined">
-          <v-card-item>
-            <div>
-              <div class="text-overline mb-1">CodeMenus</div>
-              <div class="text-h6 mb-1">コードメニュー</div>
-              <div class="text-caption">・テンプレート登録</div>
-              <div class="text-caption">・コード登録</div>
-            </div>
-          </v-card-item>
-
-          <v-card-actions>
-            <v-btn variant="outlined"> こちら </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card class="mx-auto" max-width="344" variant="outlined">
-          <v-card-item>
-            <div>
-              <div class="text-overline mb-1">OVERLINE</div>
-              <div class="text-h6 mb-1">Headline</div>
-              <div class="text-caption">
-                Greyhound divisely hello coldly fonwderfully
+    <v-container >
+      <v-row justify="start">
+        <v-col>
+          <v-card class="mx-auto" max-width="200" variant="outlined">
+            <v-card-item>
+              <div>
+                <div class="text-overline mb-1">CodeMenus</div>
+                <div class="text-h6 mb-1">コードメニュー</div>
+                <div class="text-caption">・テンプレート登録</div>
+                <div class="text-caption">・コード登録</div>
               </div>
-            </div>
-          </v-card-item>
+            </v-card-item>
 
-          <v-card-actions>
-            <v-btn variant="outlined"> Button </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+            <v-card-actions>
+              <v-btn class="mx-auto" variant="outlined" @click="toCodeMenu()">
+                こちら
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="200" variant="outlined">
+            <v-card-item>
+              <div>
+                <div class="text-overline mb-1">TreeMenus</div>
+                <div class="text-h6 mb-1">ツリーメニュー</div>
+                <div class="text-caption">・ツリー作成</div>
+                <div class="text-caption">・</div>
+              </div>
+            </v-card-item>
 
+            <v-card-actions>
+              <v-btn variant="outlined" class="mx-auto" @click="toTreeMenu()">
+                こちら
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
     <div>
       <h3>共通</h3>
       <ul>
@@ -77,5 +91,3 @@
   </div>
 </template>
 
-<script>
-</script>
